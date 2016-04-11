501fe91f4f1f67277e096c14341caa1d428d0b44
as <as@ass-MacBook-Pro.attlocal.net>
2016-04-10 20:54:09 -0700

    first stable version

---
+++ src/Broadcaster.php
@@ -0,0 +1,52 @@
+<?php
+
+class Broadcaster
+{
+    private $api_token;
+    private $host;
+    private $debug = false;
+
+    public function __construct($api_token, $host)
+    {
+        $this->api_token   = $api_token;
+        $this->host         = $host;
+    }
+    
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
+    }
+    
+    public function broadcast($channel, $event, $data)
+    {
+        //We need to do some validation here by the end of the day
+        $client = new GuzzleHttp\Client();
+        try {
+            $res = $client->request('POST', $this->host."/api/events", [
+                'headers'        => ['Accept' => 'aplication/json'],
+                'form_params' => [
+                    'api_token'     => $this->api_token,
+                    'channel'       => $channel,
+                    'event'         => $event,
+                    'data'          => json_encode($data)
+                ]
+            ]);
+            if ($debug) {
+                var_dump($res->getBody()->getContents());
+            }
+            return true;           
+        } 
+        catch (GuzzleHttp\Exception\ClientException $e)
+        {
+            return false;
+            //do nothing
+        } 
+        catch (GuzzleHttp\Exception\ServerException $e)
+        {
+            return false;
+            //probably need to notify :)
+        }
+
+        
+    }
+}
